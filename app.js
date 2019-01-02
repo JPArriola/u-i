d01d0b6da1fb4c49c79d707f7ec62f352fdf5a65
Kenneth Le <nicolemhui@gmail.com>
2019-01-02 11:22:30 -0800

    Set up mongo database

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const db = require('./config/keys').mongoURI;
 const mongoose = require('mongoose');
+const users = require("./routes/api/users");
 
 mongoose
 .connect(db, { useNewUrlParser: true })
@@ -9,6 +10,8 @@ mongoose
 .catch(err => console.log(err));
 
 app.get("/", (req, res) => res.send("Welcome to u&i!"));
+
+app.use("/api/users", users);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
