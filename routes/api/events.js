4cefbc795b72b8eeacd2df7ec3593780bc4f1f49
Kenneth Le <nicolemhui@gmail.com>
2019-01-06 14:05:26 -0800

    Implement post route for events

--- routes/api/events.js
+++ routes/api/events.js
@@ -1,21 +1,113 @@
-// const express = require('express');
-// const router = express.Router();
-// const mongoose = require('mongoose');
-// const passport = require('passport');
+const express = require('express');
+const router = express.Router();
+const mongoose = require('mongoose');
+const passport = require('passport');
+const Event = require('../../models/Event');
+const validateEventInput = require('../../validation/events');
 
-// const Event = require('../../models/Event');
-// const validateEventInput = require('../../validation/events');
-
-// // router.get('/', (req, res) => {
-// //   Event.find()
-// //     .sort({ date: -1 })
-// //     .filter({ })
-// //     .then(events => res.json(events))
-// //     .catch(err => res.status(404).json({ noeventsfound: 'No events found' }));
+router.get('/', (req, res) => {
+  Event.find()
+    .sort({ date: -1 })
+    .filter({ })
+    .then(events => res.json(events))
+    .catch(err => res.status(404).json({ noeventsfound: 'No events found' }));
 
 
-// //   // filter by connectionCode
-// // });
+  // filter by connectionCode
+});
+
+router.get('/user/:user_id', (req, res) => {
+  let currUser = User.findById(req.params.user_id);
+  let currConnectionCode = currUser.connectionCode;
+
+  Event.find({ connectionCode: currConnectionCode })
+    .then(events => res.json(events))
+    .catch(err =>
+      res.status(404).json({ noeventsfound: 'No events found' }
+      )
+    );
+});
+
+router.get('/:id', (req, res) => {
+  Event.findById(req.params.id)
+    .then(event => res.json(event))
+    .catch(err =>
+      res.status(404).json({ noeventfound: 'No event found with that ID' })
+    );
+});
+
+router.post('/', (req, res) => {
+  const { title, authorId, date } = req.body;
+  const { errors, isValid } = validateEventInput(req.body);
+
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
+
+  User.findById(authorId)
+    .then(user => { 
+      const connectionCode = user.connectionCode;
+      const newEvent = new Event({ title, authorId, date, connectionCode });
+      newEvent.save().then(event => res.json(event));
+    });
+});
+
+//FIX ME! - want some authentication in order for specific user/author to update an event
+router.patch('/:id', (req, res) => {
+  passport.authenticate('jwt', { session: false }),
+    (req, res) => {
+      const { errors, isValid } = validateEventInput(req.title);
+
+      if (!isValid) {
+        return res.status(400).json(errors);
+      }
+
+      const newEvent = new Event({
+        title: req.body.title,
+        user: req.user.id
+      });
+
+      newEvent.save().then(event => res.json(event));
+    }
+
+  Event.findById(req.params.id)
+    .then(event => res.json(event))
+    .catch(err =>
+      res.status(404).json({ noeventfound: 'No event found with that ID' })
+    );
+});
+
+//FIX ME! - want some authentication in order for specific user/author to delete an event
+router.delete('/:id', (req, res) => {
+  passport.authenticate('jwt', { session: false }),
+    (req, res) => {
+      const { errors, isValid } = validateEventInput(req.title);
+
+      if (!isValid) {
+        return res.status(400).json(errors);
+      }
+
+      const newEvent = new Event({
+        title: req.body.title,
+        user: req.user.id
+      });
+
+      newEvent.save().then(event => res.json(event));
+    }
+  
+  // Event.findById(req.params.id)
+  //   .then(event => res.json(event))
+  //   .catch(err =>
+  //     res.status(404).json({ noeventfound: 'No event found with that ID' })
+  //   );
+});
+
+module.exports = router;
+
+
+
+
+
 
 
 // router.get('/user/:user_id', (req, res) => {
@@ -92,92 +184,3 @@
 //         return res.status(400).json(errors);
 //       }
 
-router.get('/user/:user_id', (req, res) => {
-  let currUser = User.findById(req.params.user_id);
-  let currConnectionCode = currUser.connectionCode;
-
-  Event.find({ connectionCode: currConnectionCode })
-    .then(events => res.json(events))
-    .catch(err =>
-      res.status(404).json({ noeventsfound: 'No events found' }
-      )
-    );
-});
-
-router.get('/:id', (req, res) => {
-  Event.findById(req.params.id)
-    .then(event => res.json(event))
-    .catch(err =>
-      res.status(404).json({ noeventfound: 'No event found with that ID' })
-    );
-});
-
-//FIX ME!
-router.post('/',
-  (req, res) => {
-    const { errors, isValid } = validateEventInput(req.title);
-
-    if (!isValid) {
-      return res.status(400).json(errors);
-    }
-
-    const newEvent = new Event({
-      title: req.body.title,
-      authorId: req.body.authorId
-    });
-
-    newEvent.save().then(event => res.json(event));
-  }
-);
-
-//FIX ME! - want some authentication in order for specific user/author to update an event
-router.patch('/:id', (req, res) => {
-  passport.authenticate('jwt', { session: false }),
-    (req, res) => {
-      const { errors, isValid } = validateEventInput(req.title);
-
-      if (!isValid) {
-        return res.status(400).json(errors);
-      }
-
-      const newEvent = new Event({
-        title: req.body.title,
-        user: req.user.id
-      });
-
-      newEvent.save().then(event => res.json(event));
-    }
-
-  Event.findById(req.params.id)
-    .then(event => res.json(event))
-    .catch(err =>
-      res.status(404).json({ noeventfound: 'No event found with that ID' })
-    );
-});
-
-//FIX ME! - want some authentication in order for specific user/author to delete an event
-router.delete('/:id', (req, res) => {
-  passport.authenticate('jwt', { session: false }),
-    (req, res) => {
-      const { errors, isValid } = validateEventInput(req.title);
-
-      if (!isValid) {
-        return res.status(400).json(errors);
-      }
-
-      const newEvent = new Event({
-        title: req.body.title,
-        user: req.user.id
-      });
-
-      newEvent.save().then(event => res.json(event));
-    }
-  
-//   Event.findById(req.params.id)
-//     .then(event => res.json(event))
-//     .catch(err =>
-//       res.status(404).json({ noeventfound: 'No event found with that ID' })
-//     );
-// });
-
-// module.exports = router;
