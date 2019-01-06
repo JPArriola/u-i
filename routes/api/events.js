1742afcec6da870d3f586ed82a44eaa129ce89d9
Kenneth Le <nicolemhui@gmail.com>
2019-01-06 14:31:25 -0800

    Add delete route for events

--- routes/api/events.js
+++ routes/api/events.js
@@ -52,11 +52,10 @@ router.patch('/:id', (req, res) => {
     });
 });
 
-//FIX ME! - want some authentication in order for specific user/author to delete an event
 router.delete('/:id', (req, res) => {
   Event.findById(req.params.id)
     .then(event => {
-      res.json(event)
+      event.remove().then(event => res.json(event));
     })
     .catch(err =>
       res.status(404).json({ noeventfound: 'No event found with that ID' })
@@ -64,84 +63,3 @@ router.delete('/:id', (req, res) => {
 });
 
 module.exports = router;
-
-
-
-
-
-
-
-// router.get('/user/:user_id', (req, res) => {
-//   let currUser = User.findById(req.params.user_id);
-//   let currConnectionCode = currUser.connectionCode;
-
-//   Event.find({ connectionCode: currConnectionCode })
-//     .then(events => res.json(events))
-//     .catch(err =>
-//       res.status(404).json({ noeventsfound: 'No events found' }
-//       )
-//     );
-// });
-
-// router.get('/:id', (req, res) => {
-//   Event.findById(req.params.id)
-//     .then(event => res.json(event))
-//     .catch(err =>
-//       res.status(404).json({ noeventfound: 'No event found with that ID' })
-//     );
-// });
-
-// //FIX ME!
-// router.post('/',
-//   passport.authenticate('jwt', { session: false }),
-//   (req, res) => {
-//     const { errors, isValid } = validateEventInput(req.title);
-
-//     if (!isValid) {
-//       return res.status(400).json(errors);
-//     }
-
-//     const newEvent = new Event({
-//       title: req.body.title,
-//       user: req.user.id
-//     });
-
-//     newEvent.save().then(event => res.json(event));
-//   }
-// );
-
-// //FIX ME! - want some authentication in order for specific user/author to update an event
-// router.patch('/:id', (req, res) => {
-//   passport.authenticate('jwt', { session: false }),
-//     (req, res) => {
-//       const { errors, isValid } = validateEventInput(req.title);
-
-//       if (!isValid) {
-//         return res.status(400).json(errors);
-//       }
-
-//       const newEvent = new Event({
-//         title: req.body.title,
-//         user: req.user.id
-//       });
-
-//       newEvent.save().then(event => res.json(event));
-//     }
-
-//   Event.findById(req.params.id)
-//     .then(event => res.json(event))
-//     .catch(err =>
-//       res.status(404).json({ noeventfound: 'No event found with that ID' })
-//     );
-// });
-
-// //FIX ME! - want some authentication in order for specific user/author to delete an event
-// router.delete('/:id', (req, res) => {
-//   passport.authenticate('jwt', { session: false }),
-//     (req, res) => {
-//       const { errors, isValid } = validateEventInput(req.title);
-
-//       if (!isValid) {
-//         return res.status(400).json(errors);
-//       }
-
