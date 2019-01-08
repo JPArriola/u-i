51002594d1b6687d710dfa30b5a1fed5fbaa066b
Kenneth Le <nicolemhui@gmail.com>
2019-01-08 11:08:07 -0800

    Remove debugger and console log

--- frontend/src/components/albums/albums.jsx
+++ frontend/src/components/albums/albums.jsx
@@ -9,9 +9,6 @@ class Albums extends React.Component {
 	render() {
 		if (!this.props.albums) return null;
 		let album = this.props.albums.map((album, i) => {
-
-			console.log("albums props", this.props.albums)
-
 			return (
 				<AlbumItem album={album}
 					key={i}
